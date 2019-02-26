1e7204e607621bb23403adefbe61dab03f3f2837
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-02-26 21:57:42 +0900

    login component

--- nuber-clinet/src/Routes/Login/LoginPresenter.tsx
+++ nuber-clinet/src/Routes/Login/LoginPresenter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { RouteComponentProps } from 'react-router-dom';
+import Helmet from "react-helmet";
+import { Link, RouteComponentProps } from 'react-router-dom';
 import bgImage from "../../images/bg.png";
 import styled from "../../typed-components";
 
@@ -43,6 +44,7 @@ const FakeInput = styled.div`
 
 const PhoneLogin = styled.div`
   padding: 20px;
+  cursor: pointer;
 `;
 
 const Grey = styled.span`
@@ -53,6 +55,7 @@ const Grey = styled.span`
 const SocialLogin = styled.div`
   border-top: 1px solid ${props => props.theme.greyColor};
   padding: 30px 20px;
+  cursor: pointer;
 `;
 
 const SocialLink = styled.span`
@@ -66,21 +69,28 @@ interface IProps extends RouteComponentProps<any> {}
 
 const LoginPresenter: React.SFC<IProps> = () => (
 <Container>
+  <Helmet>
+    <title>Login | Nuber</title>
+  </Helmet>
   <Header>
     <Logo>
       <Title>Uber Clone</Title>
     </Logo>
   </Header>
   <Footer>
+    <Link to={"/phone-login"}>
     <PhoneLogin>
       <Subtitle>Get moving with Nuber</Subtitle>
       <FakeInput>
       🇰🇷 +82 <Grey>Enter your mobile number</Grey>
       </FakeInput>
     </PhoneLogin>
+    </Link>
+    <Link to={"/social-login"}>
     <SocialLogin>
         <SocialLink>Or connect with social</SocialLink>
     </SocialLogin>
+    </Link>
   </Footer>
 </Container>
 );
