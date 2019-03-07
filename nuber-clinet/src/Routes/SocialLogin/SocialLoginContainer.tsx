f48a73ac87324d0497e87a669d9bd050e8e5a579
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-03-08 08:50:24 +0900

    SocialLogin with Apollo

--- nuber-clinet/src/Routes/SocialLogin/SocialLoginContainer.tsx
+++ nuber-clinet/src/Routes/SocialLogin/SocialLoginContainer.tsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { Mutation, MutationFn } from 'react-apollo';
+import { RouteComponentProps } from 'react-router';
+import { facebookConnect, facebookConnectVariables } from '../../types/api';
 import SocialLoginPresenter from "./SocialLoginPresenter";
+import { FACEBOOK_CONNECT } from "./SocialLoginQueries";
 
-class SocialLoginContainer extends React.Component {
+class LoginMutation extends Mutation<
+  facebookConnect,
+  facebookConnectVariables
+>{}
+
+interface IState{
+  firstName: string;
+  lastName: string;
+  email?: string;
+  fbId: string;
+}
+
+interface IProps extends RouteComponentProps<any>{}
+
+class SocialLoginContainer extends React.Component<IProps, IState> {
+  public mutation: MutationFn;
   public render() {
-    return <SocialLoginPresenter />;
+    const { firstName, lastName, email, fbId } = this.state;
+    return (
+      <LoginMutation 
+        mutation={FACEBOOK_CONNECT} 
+        variables={{
+          email,
+          fbId,
+          firstName,
+          lastName
+        }}>
+        {(facebookConnect, {loading}) => {
+          this.mutation = facebookConnect;
+          return <SocialLoginPresenter loginCallback ={this.callback} />;
+        }
+        }
+      </LoginMutation>
+    );
+  }
+  public callback = fbData => {
+    this.setState({
+      email: fbData.email
+    });
+    this.mutation();
   }
 }
 
