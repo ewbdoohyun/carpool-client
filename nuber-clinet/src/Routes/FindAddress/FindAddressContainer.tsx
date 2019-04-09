14a3907adc88f2c20f8b3cf54923ccdfc2467539
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-04-09 12:06:30 +0900

    map event

--- nuber-clinet/src/Routes/FindAddress/FindAddressContainer.tsx
+++ nuber-clinet/src/Routes/FindAddress/FindAddressContainer.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import FindAddressPresenter from './FindAddressPresenter';
 
+interface IState {
+  lat: number;
+  lng: number;
+}
 
-
-class FindAddressContainer extends React.Component<any>{
+class FindAddressContainer extends React.Component<any,IState>{
   public mapRef: any;
   public map: google.maps.Map;
 
@@ -14,10 +17,52 @@ class FindAddressContainer extends React.Component<any>{
   }
 
   public componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeoSucces,
+      this.handleGeoError
+      );
+  }
+
+  public handleGeoSucces = (position: Position) => {
+    const {
+      coords: {latitude, longitude} 
+    } =  position;
+    this.setState({
+      lat: latitude,
+      lng: longitude
+    })
+    this.loadMap(latitude,longitude);
+  };
+
+  public handleGeoError = () => {
+    console.log("No loacation");
+  }
+
+  public loadMap = (lat, lng) => {
     const { google } = this.props;
     const maps = google.maps;
     const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
-    this.map = new maps.Map(mapNode);
+    const mapConfig: google.maps.MapOptions ={
+      center:{
+        lat,
+        lng        
+      },
+      disableDefaultUI: false,
+      zoom:11,
+
+    };
+    this.map = new maps.Map(mapNode,mapConfig);
+    this.map.addListener("dragend",this.handleDragEnd)
+  }
+  
+  public handleDragEnd = () => {
+    const newCenter = this.map.getCenter();
+    const lat = newCenter.lat();
+    const lng = newCenter.lng();
+    this.setState({
+      lat,
+      lng
+    })
   }
 
   public render(){
--- nuber-clinet/src/Routes/FindAddress/FindAddressPresenter.tsx
+++ nuber-clinet/src/Routes/FindAddress/FindAddressPresenter.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
-import styled from '../../typed-components';
+import styled from "../../typed-components";
 
 const Map = styled.div`
   position: absolute;
@@ -8,23 +8,39 @@ const Map = styled.div`
   left: 0;
   height: 100%;
   width: 100%;
+  z-index: 1;
+
 `;
 
-interface IProps{
+const Center = styled.div`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  z-index: 2;
+  font-size: 30px;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+interface IProps {
   mapRef: any;
 }
 
-class FindAddressPresenter extends React.Component<IProps>{
-  public render(){
+class FindAddressPresenter extends React.Component<IProps> {
+  public render() {
     const { mapRef } = this.props;
     return (
       <div>
         <Helmet>
           <title>Find Address | Nuber</title>
         </Helmet>
-        <Map innerRef={mapRef} />
+        <Center>📍</Center>
+        <Map ref={mapRef} />
       </div>
-    )
+    );
   }
 }
 
