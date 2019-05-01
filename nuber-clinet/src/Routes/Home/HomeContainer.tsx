823b2390a560ffe99985668f9dd71d6879f2c32d
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-05-01 18:27:11 +0900

    Ride screening

--- nuber-clinet/src/Routes/Home/HomeContainer.tsx
+++ nuber-clinet/src/Routes/Home/HomeContainer.tsx
@@ -146,7 +146,10 @@ class HomeContainer extends React.Component<IProps, IState> {
                         subscribeToMore(rideSubscriptionOptions);
                       }
                       return (
-                        <AcceptRide mutation={ACCEPT_RIDE}>
+                        <AcceptRide 
+                          mutation={ACCEPT_RIDE}
+                          onCompleted={this.handleRideAcceptance}
+                        >
                           {acceptRideFn => (
                             <HomePresenter
                             loading={loading}
@@ -398,9 +401,11 @@ class HomeContainer extends React.Component<IProps, IState> {
     }
   };
   public handleRideRequest = (data: requestRide) => {
+      const { history } = this.props;
       const { RequestRide } = data;
       if(RequestRide.ok){
         toast.success("Drive Requested, finding a driver");
+        history.push(`/ride/${RequestRide.ride!.id}`);
       }else{
         toast.error(RequestRide.error);
       }
@@ -417,11 +422,17 @@ class HomeContainer extends React.Component<IProps, IState> {
         });
       }
     }
+  };
+  public handleRideAcceptance = (data: acceptRide) => {
+    const { history } = this.props;
+    const { UpdateRideStatus } = data;
+    console.log("Accept ride");
+    console.log(UpdateRideStatus);
+    if (UpdateRideStatus.ok){
+      history.push(`/ride/${UpdateRideStatus.rideId}`);
+    }
   }
 
-  public handleSubscriptionUpdate = data => {
-    console.log(data);
-  }
 }
 
 export default graphql<any, reportMovement, reportMovementVariables>(
