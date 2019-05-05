16aeeab210a116fdde944e085f0428fcb470b298
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-05-05 14:19:12 +0900

    during getRideQuery

--- nuber-clinet/src/Routes/Ride/RideContainer.tsx
+++ nuber-clinet/src/Routes/Ride/RideContainer.tsx
@@ -1,11 +1,21 @@
+import { SubscribeToMoreOptions } from "apollo-client";
 import React from "react";
-import { Query } from "react-apollo";
-import { getRide, getRideVariables } from '../../types/api';
+import { Query, Mutation } from "react-apollo";
+import { 
+  getRide, 
+  getRideVariables,  
+  updateRide, 
+  updateRideVariables,
+  userProfile 
+} from '../../types/api';
 import { RouteComponentProps } from "react-router-dom";
 import RidePresenter from "./RidePresenter";
-import { GET_RIDE } from './RideQueries';
+import { GET_RIDE, RIDE_SUBSCRIPTION, UPDATE_RIDE_STATUS } from "./RideQueries";
+import { USER_PROFILE } from '../../sharedQueries';
 
 class RideQuery extends Query<getRide,getRideVariables>{};
+class ProfileQuery extends Query<userProfile>{};
+class RideUpdate extends Mutation<updateRide, updateRideVariables>{};
 
 interface IProps extends RouteComponentProps<any> {}
 
@@ -19,13 +29,43 @@ class RideContainer extends React.Component<IProps> {
   public render() {
     const {
       match: {
-        params: {rideId}
+        params: { rideId }
       }
     } = this.props;
     return (
-      <RideQuery query={GET_RIDE} variables={{ rideId }}>
-        {data => <RidePresenter data={data} />}
-      </RideQuery>
+      <ProfileQuery query={USER_PROFILE}>
+        {({ data: userData }) => (
+          <RideQuery query={GET_RIDE} variables={{ rideId }}>
+            {({ data, loading, subscribeToMore }) => {
+              const subscribeOptions: SubscribeToMoreOptions = {
+                document: RIDE_SUBSCRIPTION,
+                updateQuery: (prev, { subscriptionData }) => {
+                  if (!subscriptionData.data) {
+                    return prev;
+                  }
+                  console.log(prev, subscriptionData);
+                }
+              };
+              subscribeToMore(subscribeOptions);
+              return (
+                <RideUpdate
+                  mutation={UPDATE_RIDE_STATUS}
+                  refetchQueries={GET_RIDE}
+                >
+                  {updateRideFn => (
+                    <RidePresenter
+                      userData={userData}
+                      loading={loading}
+                      data={data}
+                      updateRideFn={updateRideFn}
+                    />
+                  )}
+                </RideUpdate>
+              );
+            }}
+          </RideQuery>
+        )}
+      </ProfileQuery>
     );
   }
 }
--- nuber-clinet/src/Routes/Ride/RidePresenter.tsx
+++ nuber-clinet/src/Routes/Ride/RidePresenter.tsx
@@ -1,13 +1,106 @@
 import React from "react";
 import styled from "../../typed-components";
-import { getRide } from '../../types/api';
+import { getRide, userProfile } from '../../types/api';
+import Button from '../../Components/Button';
+import { MutationFn } from 'react-apollo';
+import { Link } from 'react-router-dom';
 
-const Container = styled.div``;
+const Container = styled.div`
+
+`;
+
+const Title = styled.h4`
+`;
+
+const Data = styled.span`
+`;
+
+const Img = styled.img`
+`;
+
+const Passenger = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Buttons = styled.div`
+`;
+
+const ExtendedButton = styled(Button)`
+`;
 
 interface IProps {
   data?: getRide;
+  userData?: userProfile;
+  loading: boolean;
+  updateRideFn: MutationFn;
 }
 
-const RidePresenter: React.SFC<IProps> = () => <Container>Ride</Container>;
+const RidePresenter: React.SFC<IProps> = ({
+ data,
+ userData,
+ updateRideFn 
+}) => (
+  <Container>
+    { data && data.GetRide && data.GetRide.ride &&
+      userData && userData.GetMyProfile && userData.GetMyProfile.user && 
+    (
+        <React.Fragment>
+          <Title>Passenger</Title>
+          <Passenger>
+            <Img src={data.GetRide.ride.passenger.profilePhoto!} />
+            <Data>{data.GetRide.ride.passenger.fullName!}</Data>
+          </Passenger>
+          {data.GetRide.ride.driver && (
+            <React.Fragment>
+              <Title>Driver</Title>
+              <Passenger>
+                <Img src={data.GetRide.ride.passenger.profilePhoto!} />
+                <Data>{data.GetRide.ride.passenger.fullName!}</Data>
+              </Passenger>
+            </React.Fragment>
+          )}
+          <Title>From</Title>
+          <Data>{data.GetRide.ride.pickUpAddress}</Data>
+          <Title>To</Title>
+          <Data>{data.GetRide.ride.dropOffAddress}</Data>
+          <Title>Price</Title>
+          <Data>{data.GetRide.ride.price}</Data>
+          <Title>Distance</Title>
+          <Data>{data.GetRide.ride.distance}</Data>
+          <Title>Duration</Title>
+          <Data>{data.GetRide.ride.duration}</Data>
+          <Title>Status</Title>
+          <Data>{data.GetRide.ride.status}</Data>
+          <Buttons>
+            { data.GetRide.ride.driver.id === userData.GetMyProfile.user.id &&
+              data.GetRide.ride.status === "ONROUTE" &&  data.GetRide.ride.id &&(
+                <ExtendedButton
+                  value={"Finished"}
+                  onClick={() =>
+                    updateRideFn({
+                      variables: {
+                        rideId: data.GetRide.ride ? data.GetRide.ride.id : null,
+                        status: "FINISHED"
+                      }
+                    })
+                  }
+                />
+              )}
+            {data.GetRide.ride.driver.id === userData.GetMyProfile.user.id ||
+              ( data.GetRide.ride.passenger.id === userData.GetMyProfile.user.id &&
+                data.GetRide.ride.status === "ACCEPTED" && (
+                  <Link to={`/chat/${data.GetRide.ride.chatId}`}>
+                    <ExtendedButton value={"Chat"} onClick={null} />
+                  </Link>
+                )
+              )
+            }
+          </Buttons>
+        </React.Fragment>
+    )}
+  </Container>
+);
 
 export default RidePresenter;
